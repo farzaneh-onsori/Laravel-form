8e72bccb62e17d1e9a92a5a9b079a32a12d9bf18
Farzaneh Onsori <farzaneh.onsori@yahoo.com>
2022-01-07 15:57:45 +0100

    Layout of a real template

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,19 @@
 @extends('layouts.master')
 
-@section('title' , 'About Page')
 
 @section('content')
 
 <h2>About Page</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, veniam id voluptatem voluptas numquam eligendi voluptatibus inventore non alias quibusdam porro soluta, tempore expedita! Mollitia exercitationem minus inventore dolorem. Delectus.</p>
 
 @endsection
 
-
+@section('sidebar')
+    @parent
+    <!-- Side Widget -->
+  <div class="card my-4">
+    <h5 class="card-header">About Widget</h5>
+    <div class="card-body">
+      You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+    </div>
+  </div>
+@endsection
